f4be2489af5dbdb2e7c8321fc3e4112ab0cfe037
Artem Vorobyev <Djagernayt1999@mail.ru>
2019-04-22 18:08:33 +0300

    added mongodb

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const fetch = require('isomorphic-fetch');
 const app = express();
+const MongoClient = require("mongodb").MongoClient;
+
+const url = "mongodb://localhost:27017/";
+const dbName = 'userdb';
+
+const mongoClient = new MongoClient(url, { useNewUrlParser: true, auth: {
+    user: 'root',
+    password: 'example',
+    }});
+
+mongoClient.connect(function (err) {
+    if (err) {
+        console.log(err);
+    }
+    console.log("Connected");
+    const db = mongoClient.db(dbName);
+});
 
 const listCurrency = ['AUD', 'AZN', 'GBP', 'AMD', 'BYN', 'BGN', 'BRL', 'HUF', 'HKD', 'DKK', 'USD', 'EUR', 'INR', 'KZT',
-                        'CAD', 'KGS', 'CNY', 'MDL', 'NOK', 'PLN', 'RON', 'XDR', 'SGD', 'TJS', 'TRY', 'TMT', 'UZS',
-                        'UAH', 'CZK', 'SEK', 'CHF', 'ZAR', 'KRW', 'JPY'];
+    'CAD', 'KGS', 'CNY', 'MDL', 'NOK', 'PLN', 'RON', 'XDR', 'SGD', 'TJS', 'TRY', 'TMT', 'UZS',
+    'UAH', 'CZK', 'SEK', 'CHF', 'ZAR', 'KRW', 'JPY'];
 
 app.get('/', function (req, res) {
     fetch('https://www.cbr-xml-daily.ru/daily_json.js')
@@ -17,6 +34,8 @@ app.get('/', function (req, res) {
                             return {
                                 name: currency[cur].Name,
                                 value: currency[cur].Value,
+                                nominal: currency[cur].Nominal,
+                                charcode: currency[cur].CharCode,
                             }
                         });
                         let bodyContent = '<html>' +
