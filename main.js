3e8a482b734febf2a0bd4adf3559f24073ab0061
Artem Vorobyev <Djagernayt1999@mail.ru>
2019-04-22 23:52:10 +0300

    added currency db

--- main.js
+++ main.js
@@ -17,57 +17,84 @@ mongoClient.connect(function (err) {
     }
     console.log("Connected");
     const db = mongoClient.db(dbName);
-});
 
-const listCurrency = ['AUD', 'AZN', 'GBP', 'AMD', 'BYN', 'BGN', 'BRL', 'HUF', 'HKD', 'DKK', 'USD', 'EUR', 'INR', 'KZT',
-    'CAD', 'KGS', 'CNY', 'MDL', 'NOK', 'PLN', 'RON', 'XDR', 'SGD', 'TJS', 'TRY', 'TMT', 'UZS',
-    'UAH', 'CZK', 'SEK', 'CHF', 'ZAR', 'KRW', 'JPY'];
 
-app.get('/', function (req, res) {
-    fetch('https://www.cbr-xml-daily.ru/daily_json.js')
-        .then(response => {
-            if (response.ok) {
-                response.json()
-                    .then(data => {
-                        let currency = data.Valute;
-                        let cleanedData = listCurrency.map(cur => {
-                            return {
-                                name: currency[cur].Name,
-                                value: currency[cur].Value,
-                                nominal: currency[cur].Nominal,
-                                charcode: currency[cur].CharCode,
+    const listCurrency = ['AUD', 'AZN', 'GBP', 'AMD', 'BYN', 'BGN', 'BRL', 'HUF', 'HKD', 'DKK', 'USD', 'EUR', 'INR', 'KZT',
+        'CAD', 'KGS', 'CNY', 'MDL', 'NOK', 'PLN', 'RON', 'XDR', 'SGD', 'TJS', 'TRY', 'TMT', 'UZS',
+        'UAH', 'CZK', 'SEK', 'CHF', 'ZAR', 'KRW', 'JPY'];
+
+    app.get('/', function (req, res) {
+        fetch('https://www.cbr-xml-daily.ru/daily_json.js')
+            .then(response => {
+                if (response.ok) {
+                    response.json()
+                        .then(data => {
+                                let currency = data.Valute;
+                                let cleanedData = listCurrency.map(cur => {
+                                    return {
+                                        name: currency[cur].Name,
+                                        value: currency[cur].Value,
+                                        nominal: currency[cur].Nominal,
+                                        charcode: currency[cur].CharCode,
+                                    }
+                                });
+                                const collection = db.collection('documents');
+                                let one = cleanedData.forEach(function (elem) {
+                                    let time = new Date();
+                                    let formatTime = time.toISOString();
+                                    try {
+                                        collection.insertOne({
+                                            name: elem.name,
+                                            value: elem.value,
+                                            charcode: elem.charcode,
+                                            nominal: elem.nominal,
+                                            date: formatTime,
+                                        }), function (err, result) {
+                                            if (err) {
+                                                console.log(err);
+                                            } else {
+                                                console.log(result);
+                                            }
+                                        }
+                                    } catch (e) {
+                                        console.log(e);
+                                    }
+                                    try {
+                                    MongoClient.close();
+                                    } catch (e) {
+                                        console.log(e);
+                                    }
+                                    });
+                                let bodyContent = '<html>' +
+                                    '<body>' +
+                                    '<table>' +
+                                    cleanedData.reduce((pv, cv) => {
+                                        return pv + '' +
+                                            '<tr>' +
+                                            '<td>' +
+                                            cv.name +
+                                            '</td>' +
+                                            '<td>' +
+                                            cv.value +
+                                            '</td>' +
+                                            '</tr>'
+                                    }, '') +
+                                    '</table>' +
+                                    '</body>' +
+                                    '</html>';
+                                res.status(200).send(bodyContent);
                             }
-                        });
-                        let bodyContent = '<html>' +
-                            '<body>' +
-                            '<table>' +
-                            cleanedData.reduce((pv, cv) => {
-                               return pv + '' +
-                                   '<tr>' +
-                                   '<td>' +
-                                   cv.name +
-                                   '</td>' +
-                                   '<td>' +
-                                   cv.value +
-                                   '</td>' +
-                                   '</tr>'
-                            },'') +
-                            '</table>' +
-                            '</body>' +
-                            '</html>';
-                        res.status(200).send(bodyContent);
-
-                    })
-            }
-        })
-        .catch(error => {
-            res.json({
-                status: 'error',
-                description: "Can't connect to https://www.cbr-xml-daily.ru/daily_json.js"
+                        )
+                }
+            })
+            .catch(error => {
+                res.json({
+                    status: 'error',
+                    description: "Can't connect to https://www.cbr-xml-daily.ru/daily_json.js"
+                });
             });
-        });
+    });
 });
-
 
 app.listen(5000, function () {
     console.log('Example app listening on port 5000!');
